c435278d2d2b69f615c9a4c01ddba2d2cb53c8bf
ny25mk <ny25mk0419@gmail.com>
2019-08-27 16:43:21 +0900

    [add] ギミックの生成部分の作成
    ダルマとバランスボールおばさんの2つを生成できるようにしました

---
+++ Assets/Nagashima/Scripts/BalanceBallGimmick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceBallGimmick : BaseGimmick
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        base.OnUpdate();
+
+        Move();
+
+    }
+
+    public void Move()
+    {
+
+        transform.Translate(-0.05f, 0.0f, 0.0f);
+
+    }
+
+}
---
+++ Assets/Nagashima/Scripts/BaseGimmick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseGimmick : MonoBehaviour
+{
+    [SerializeField]
+    protected int maxActiveTime;      //そのオブジェクトの活動可能時間
+    protected int activeTimer;        //現在の活動時間
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        activeTimer = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        OnUpdate();
+
+    }
+
+    public virtual void OnUpdate()
+    {
+
+        activeTimer++;
+
+        //時間経過で削除
+        if (activeTimer > maxActiveTime)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("寿命があぁぁ");
+            return;
+        }
+
+    }
+
+}
